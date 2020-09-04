87c8b6712f55164a81578fa04dc0fce45c099d8d
Rajkumar Patel <rajkumar.patel@seagate.com>
2020-09-04 00:18:50 -0600

    EOS-12578 : Add copyright to motr-galois repo
    Rule for copyright change -
    
    Updated files : Add Seagate mention along with original Author
    
    Signed-off-by: Rajkumar Patel <rajkumar.patel@seagate.com>

--- src/gf_ilog.c
+++ src/gf_ilog.c
@@ -1,5 +1,7 @@
 /* gf_ilog.c
  * James S. Plank
+ * And
+ * Seagate Technology LLC and/or its Affiliates
 
 Galois.tar - Fast Galois Field Arithmetic Library in C/C++
 Copright (C) 2007 James S. Plank
@@ -23,6 +25,11 @@ Department of Computer Science
 University of Tennessee
 Knoxville, TN 37996
 plank@cs.utk.edu
+
+And
+
+Seagate Technology LLC and/or its Affiliates
+opensource@seagate.com or cortx-questions@seagate.com
 
  */
 
